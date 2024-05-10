1a757a7e58a4956090b3b377b863c21fd279361a
Paxton <paxton@drab.org>
2024-05-09 19:26:10 -0500

    realise everything was a lie and 20 lines of code means nothing i just needed to use a modulo and now i am crying tears of confusion

--- universal_gyro.py
+++ universal_gyro.py
@@ -5,43 +5,14 @@ class Universal_Gyro:
     self.heading = 0
     self.angle = 0
 
-    def __in_range(self, low, high, target):
+  def __in_range(self, low, high, target):
     if target >= low and target <= high:
       return True
     else:
       return False
 
   def find_nearest_heading(self, target_angle):
-    possible_values = []
-    # POSITIVE NUMBERS
-    if target_angle > 0:
-      if self.__in_range(0, 360, target_angle):
-        print(target_angle, '<--')
-        return target_angle
-
-      for x in range(1, 360):
-        if target_angle % x == 0:
-          possible_values.append(target_angle / x)
-
-      for value in possible_values:
-        if target_angle - value == 360:
-          print(value, '<--')
-          return value
-    
-    # NEGATIVE NUMBERS
-    elif target_angle < 0:
-      if self.__in_range(-360, 0, target_angle):
-        print(target_angle + 360, '<--')
-        return target_angle + 360
-    
-      for x in range(1, 360):
-        if target_angle % x == 0:
-          possible_values.append(target_angle / x)
-
-      for value in possible_values:
-        if target_angle - value == -360:
-          print(value + 360, '<--')
-          return value + 360
+    return target_angle % 360
 
   def heading(self):
     return self.heading
@@ -49,10 +20,7 @@ class Universal_Gyro:
   def angle(self):
     return self.angle
 
-  def update_angle(self, new_angle):
-    self.angle = new_angle
-    return self.angle
-
-  def update_heading(self, new_heading):
+  def update(self, angle):
+    self.angle = angle
     self.heading = self.find_nearest_heading(self.angle)
-    return self.heading  
+    return self.angle, self.heading
