0d4a74c7f0a2633fe6ef2f55e8eacb32e534a0bc
Corwin Dennis <corwon52@gmail.com>
2024-12-23 17:47:53 -0600

    fix m03

--- M03.py
+++ M03.py
@@ -2,9 +2,8 @@ import Commands
 
 def m03():
   return [
-    # # Push out
-    # Commands.GyroDrive(speed=75, distance=100),
-    # # Return to home
-    # Commands.DriveMM(speed=75, distance=-100),
-    Commands.ActMotorTime(speed=100, time=3000, motor=2)
+    # Push out
+    Commands.GyroDrive(speed=75, distance=100),
+    # Return to home
+    Commands.DriveMM(speed=75, distance=-100),
   ]
