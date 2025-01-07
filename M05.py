2969cb71d393e2be6b6cfc4140e16b05b0f89c23
Zane Elingson <zane@34334.net>
2025-01-07 16:27:42 -0600

    m05 start

--- M05.py
+++ M05.py
@@ -2,48 +2,7 @@ import Commands
 
 def m05():
   return [
-    # Start
-    Commands.GyroDrive(speed=200, distance=395),
-    # Turn to sub station
-    Commands.Pivot(angle=-48.5),
-    # Drive to sub station
-    Commands.GyroDrive(speed=200, distance=575),
-    # Slow Down
-    Commands.GyroDrive(speed=100, distance=25),
-    # LineSquare
-    Commands.LineSquare(approachSpeed=100, finetuneSpeed=50, returnTime=4500),
-    # Backup
-    Commands.DriveMM(speed=80, distance=-15),
-    # Turn to angler fish
-    Commands.Pivot(speed=80, angle=-71),
-    # drive to angler fish
-    Commands.GyroDrive(speed=175, distance=246),
-    # Scare it away
-    Commands.Pivot(speed=100, angle=20),
-    # Wait to prevent springing
-    Commands.Wait(time=200),
-    # Double check
-    Commands.GyroDrive(speed=150, distance=10),
-    # Backup
-    Commands.DriveMM(speed=150, distance=-10),
-    # Turn to coral
-    Commands.Pivot(speed=100, angle=35),
-    # Lower hook
-    Commands.ActMotorTime(motor='right', speed=600, time=1100, wait=False),
-    # Drive to the coral
-    Commands.GyroDrive(speed=200, distance=320),
-    # Agressavly hit coral untill it revives it somehow
-    Commands.ActMotorTime(motor='right', speed=600, time=1250),
-    # Up
-    Commands.ActMotorTime(motor='right', speed=-500, time=1850, wait=False),
-    # Backup
-    Commands.DriveMM(speed=200, distance=-90),
-    # One wheel turn
-    Commands.DriveMotor(motor="right", angle=168, speed=185),
-    # Scoop up all nearby wildlife
-    Commands.GyroDrive(speed=200, distance=300),
-    # Turn to home
-    Commands.Pivot(angle=-40),
-    # Return to home
-    Commands.DriveMM(speed=222, distance=800),
+    Commands.GyroDrive(speed=-175, distance=90),
+    Commands.GyroPivot(angle=-56),
+    Commands.GyroDrive(speed=-175, distance=1050),
   ]
