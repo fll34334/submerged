6cc4448d5c52b73bd6b369a02c2da0d4389970c9
Aeron Grey <aeron@34334.net>
2025-01-09 12:10:52 -0800

    work on mo5

--- M05.py
+++ M05.py
@@ -4,5 +4,7 @@ def m05():
   return [
     Commands.GyroDrive(speed=-175, distance=90),
     Commands.GyroPivot(angle=-56),
-    Commands.GyroDrive(speed=-175, distance=1050),
+    Commands.GyroDriveREG(speed=-175, distance=500),
+    Commands.GyroPivot(angle=13),
+    Commands.GyroDrive(speed=-175, distance=250),
   ]
