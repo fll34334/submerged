4221190409bdf62c20dffa4437a948a37e5bbcb7
Aeron Grey <aeron@34334.net>
2025-01-09 15:12:19 -0800

    needs work

--- M02.py
+++ M02.py
@@ -2,13 +2,13 @@ import Commands
 
 def m02():
   return [
-    Commands.GyroDrive(speed=250, distance=400),
-    Commands.GyroPivot(angle=47),
-    Commands.GyroDrive(speed=250, distance=125),
-    #raise captain hook
-    #lower captain hook
-    #back up
-    #raise captain hook
-    #lower captain hook
-    #easton
+    Commands.GyroDrive(speed=175, distance=620),
+    Commands.ActMotorTime(motor=1, speed=250, time=3000),
+    Commands.GyroDrive(speed=175, distance=8),
+    Commands.GyroPivot(angle=45),
+    Commands.ActMotorTime(motor=1, speed=250, time=4000),
+    Commands.ActMotorTime(motor=1, speed=-250, time=7000),
+    #add hook code
+    Commands.GyroPivot(angle=135),
+    Commands.GyroDrive(speed=175, distance=300),
     ]
