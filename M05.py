90cbdeddeaf7f47becd55294de7a2d2261c9006a
Corwin Dennis <corwon52@gmial.com>
2025-01-09 18:55:29 -0600

    m05 save fix

--- M05.py
+++ M05.py
@@ -2,14 +2,8 @@ import Commands
 
 def m05():
   return [
-<<<<<<< HEAD
-    Commands.GyroDrive(speed=-175, distance=90),
-    Commands.GyroPivot(angle=-57),
-    Commands.GyroDriveREG(speed=-150, distance=750),
-=======
     Commands.GyroDrive(speed=-175, distance=50),
     Commands.GyroPivot(angle=-55),
     Commands.GyroDrive(speed=-175, distance=490),
     Commands.GyroDrive(speed=-175, distance=350, angle=3),
->>>>>>> d201e84 (m05 better (not))
   ]
