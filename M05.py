bc23fd620667ece94067f0de31310f2cb55c191b
Corwin Dennis <corwon52@gmial.com>
2025-01-11 17:33:27 -0600

    m05 cant work

--- M05.py
+++ M05.py
@@ -2,11 +2,12 @@ import Commands
 
 def m05():
   return [
-    Commands.GyroDrive(speed=-175, distance=50),
-    Commands.GyroPivot(angle=-55),
+    Commands.GyroDrive(speed=-150, distance=20),
+    Commands.GyroPivot(angle=-56),
     Commands.GyroDrive(speed=-175, distance=490),
-    Commands.GyroPivot(angle=6),
+    #Commands.GyroPivot(angle=3),
     Commands.GyroDrive(speed=-175, distance=400),
-    Commands.ActMotorTime(motor=2, speed=200, time=1800),
-    Commands.ActMotorTime(motor=2, speed=-200, time=1800),
+    #Commands.GyroPivot(angle=-36),
+    # Commands.ActMotorTime(motor=2, speed=200, time=1500),
+    # Commands.ActMotorTime(motor=2, speed=-300, time=2500),
   ]
