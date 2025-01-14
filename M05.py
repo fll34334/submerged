d6a512e21c7f0c715816aa847c70420002e2111a
Christina <Christina@34334.net>
2025-01-13 20:06:08 -0600

    m05 progress

--- M05.py
+++ M05.py
@@ -2,12 +2,12 @@ import Commands
 
 def m05():
   return [
-    Commands.GyroDrive(speed=-150, distance=20),
-    Commands.GyroPivot(angle=-56),
+    Commands.GyroDrive(speed=-150, distance=30),
+    Commands.GyroPivot(angle=-51),
     Commands.GyroDrive(speed=-175, distance=490),
-    #Commands.GyroPivot(angle=3),
     Commands.GyroDrive(speed=-175, distance=400),
-    #Commands.GyroPivot(angle=-36),
-    # Commands.ActMotorTime(motor=2, speed=200, time=1500),
-    # Commands.ActMotorTime(motor=2, speed=-300, time=2500),
+    Commands.ActMotorTime(motor=2, speed=200, time=1500, wait=False),
+    Commands.GyroDrive(speed=-175, distance=90),
+    Commands.GyroPivot(angle=-36)
+    Commands.ActMotorTime(motor=2, speed=-300, time=2500),
   ]
