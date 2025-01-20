dcb64fdffded196054f8992c5b02b8e51dd8edd8
Corwin <corwon52@gmail.com>
2025-01-20 15:54:56 -0600

    m05 with m13, and m03 fix

--- M05.py
+++ M05.py
@@ -5,30 +5,28 @@ def m05():
     # Drive from angled jig
     Commands.GyroDrive(speed=-175, distance=910, angle=1),
     # Turn around
-    Commands.GyroPivot(angle=143),
+    Commands.GyroPivot(angle=144),
     # Line up
     Commands.GyroDrive(speed=-175, distance=90),
     # Turn To LineSquare
     Commands.GyroPivot(angle=-90),
     # LineSquare
     Commands.LineSquare(approachSpeed=75, returnTime=4000),
-    # Code for M13 bump
-    # Commands.ActMotorTime(motor=2, speed=300, time=850),
-    # Commands.GyroPivot(speed=250, angle=-58),
-    # Commands.GyroPivot(angle=58),
-    # Commands.ActMotorTime(motor=2, speed=-300, time=1000),
-    # Turn away
+    # M13 bump
+    Commands.ActMotorTime(speed=450, time=1000),
+    Commands.ActMotorTime(speed=-350, time=1800),
+    # Turn away and drop arm
     Commands.GyroPivot(speed=200, angle=-90),
-    # Drop Elephent Trunk
-    Commands.ActMotorTime(motor=2, speed=300, time=1300, wait=False),
+    Commands.ShiftGear(gear=2),
+    Commands.ActMotorTime(speed=300, time=1300, wait=False),
     # Drive to seabed
-    Commands.GyroDrive(speed=-175, distance=129),
+    Commands.GyroDrive(speed=-175, distance=143),
     # Lift sample
     Commands.ActMotorTime(motor=2, speed=-300, time=2400),
     # Drive away
-    Commands.GyroDrive(speed=-200, distance=265),
+    Commands.GyroDrive(speed=-200, distance=260),
     # One wheel turn to home
-    Commands.DriveMotor(motor="left", angle=160, speed=175),
+    Commands.DriveMotor(motor="left", angle=167, speed=175),
     # Drive to home
-    Commands.DriveMM(speed=250, distance=750),
+    Commands.DriveMM(speed=250, distance=777),
   ]
