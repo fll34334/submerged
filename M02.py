0b0a01bf3a344d1cd7f78f3744d451fdaa95bf08
Joshua Weathers <joshua@34334.net>
2025-01-20 15:36:49 -0600

    M02 better

--- M02.py
+++ M02.py
@@ -3,13 +3,17 @@ import Commands
 def m02():
   return [
     # Start Driving
-    Commands.GyroDrive(speed=175, distance=359),
+    Commands.GyroDrive(speed=175, distance=367),
     # Turn to M06
-    Commands.GyroPivot(angle=68),
+    Commands.GyroPivot(angle=65),
     # Arggresively ram with passive grabber
-    Commands.GyroDrive(speed=200, distance=135),
+    Commands.GyroDriveREG(speed=250, distance=185),
     # Back outs
-    Commands.GyroDrive(speed=175, distance=-135),
+    Commands.GyroDrive(speed=175, distance=-65),
     # Turn To align with M01
     Commands.GyroPivot(angle=90),
+    #drive to M01
+    Commands.GyroDrive (speed=175, distance=-130),
+    Commands.ActMotorTime(motor=4, speed=-400, time=1800),
+    Commands.ActMotorTime(motor=4, speed=400, time=1800),
     ]
