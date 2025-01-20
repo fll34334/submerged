d9a2f43112eec1f56e114b10235916a8bdb4c209
Joshua Weathers <joshua@34334.net>
2025-01-20 16:44:10 -0600

    M02 no work

--- M02.py
+++ M02.py
@@ -5,15 +5,22 @@ def m02():
     # Start Driving
     Commands.GyroDrive(speed=175, distance=358),
     # Turn to M06
-    Commands.GyroPivot(angle=68),
+    Commands.GyroPivot(angle=71),
     # Arggresively ram with passive grabber
-    Commands.GyroDriveREG(speed=250, distance=185),
+    Commands.GyroDriveREG(speed=175, distance=120),
+    Commands.GyroDriveREG(speed=250, distance=75),
     # Back outs
-    Commands.GyroDrive(speed=175, distance=-65),
+    Commands.GyroDrive(speed=175, distance=-90),
     # Turn To align with M01
     Commands.GyroPivot(angle=90),
     #drive to M01
-    Commands.GyroDrive (speed=175, distance=-130),
-    Commands.ActMotorTime(motor=4, speed=-400, time=1800),
-    Commands.ActMotorTime(motor=4, speed=400, time=1800),
+    Commands.GyroDrive (speed=175, distance=-145),
+    Commands.ActMotorTime(motor=4, speed=-700, time=3000),
+    Commands.ActMotorTime(motor=4, speed=700, time=3000),
+    Commands.GyroDrive(speed=175, distance=-120),
+    Commands.GyroPivot(angle=32),
+    Commands.ActMotorTime(motor=4, speed=-700, time=3000),
+    Commands.ActMotorTime(motor=4, speed=700, time=3000),
+    Commands.ActMotorTime(motor=1, speed=300, time=1000),
+    Commands.ActMotorTime(motor=1, speed=-300, time=1000),
     ]
