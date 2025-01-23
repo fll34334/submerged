3e96ef033eef6ac2b78649f2876da902e5c3dac7
Corwin <corwon52@gmail.com>
2025-01-23 15:30:09 -0600

    m05 home but not 13 or SS

--- M05.py
+++ M05.py
@@ -3,7 +3,7 @@ import Commands
 def m05():
   return [
     # Drive from angled jig
-    Commands.GyroDrive(speed=-175, distance=898),
+    Commands.GyroDrive(speed=-175, distance=892),
     # Turn around
     Commands.GyroPivot(angle=145),
     # Line up
@@ -14,13 +14,13 @@ def m05():
     Commands.LineSquare(approachSpeed=75, returnTime=4000),
     # M13 bump
     # Turn away and drop arm
-    Commands.GyroDriveREG(speed=-120, distance=-44),
+    Commands.GyroDriveREG(speed=-120, distance=-37),
     Commands.GyroPivot(angle=12),
-    Commands.ActMotorTime(speed=200, time=1600),
+    Commands.ActMotorTime(speed=250, time=1600),
     Commands.ActMotorTime(speed=-200, time=2200),
     Commands.GyroPivot(angle=-12),
-    Commands.GyroDriveREG(speed=-120, distance=15),
-    Commands.GyroPivot(angle=-90),
+    Commands.DriveMM(speed=-100, distance=12),
+    Commands.GyroPivot(angle=-91),
     Commands.ShiftGear(gear=2),
     # Drive to seabed
     Commands.GyroDrive(speed=-150, distance=90),
