110d933eb79811c52a570f530390fb55102324cd
Corwin <corwon52@gmail.com>
2025-01-24 18:11:18 -0600

    m02 state

--- M02.py
+++ M02.py
@@ -3,35 +3,38 @@ import Commands
 def m02():
   return [
     # Start Driving
-    Commands.GyroDrive(speed=175, distance=385),
+    Commands.GyroDrive(speed=175, distance=390),
     # Turn to M06
     Commands.GyroPivot(angle=68, speed=75),
     # Arggresively ram with passive grabber
-    Commands.DriveMM(speed=175, distance=-380),
-    # Back outs
-    Commands.GyroDrive(speed=175, distance=-108),
+    Commands.DriveMM(speed=175, distance=-410),
+    # Back outs while losing all sanity that you somehow still have left
+    Commands.GyroDrive(speed=175, distance=-132),
     # Turn To align with M01
     Commands.GyroPivot(angle=87, speed=75),
-    # drive to M01
-    Commands.GyroDrive(speed=175, distance=-165),
-    Commands.ActMotorTime(motor=4, speed=-350, time=8000),
-    Commands.ActMotorTime(motor=4, speed=350, time=1900),
-    Commands.DriveMM(speed=175, distance=300),
-    Commands.ActMotorTime(motor=4, speed=-350, time=7000),
-    Commands.ActMotorTime(motor=4, speed=350, time=2500),
-    Commands.ActMotorTime(speed=350, time=5500, wait=False),
-    Commands.GyroDrive(speed=-175, distance=-290, angle=-5),
-    Commands.GyroPivot(angle=18),
-    Commands.GyroDrive(speed=-175, distance=180),
-    Commands.ActMotorTime(speed=250, time=2400),
-    Commands.ActMotorTime(speed=-200, time=2400),
-    Commands.GyroDrive(speed=-175, distance=800, angle=20),
-
-    # M03 backup 3rd run (same jig as M02, useing tiger)
-    # Commands.GyroDrive(speed=-175, distance=385),
-    # Commands.GyroPivot(angle=22, speed=75),
-    # Commands.GyroDrive(speed=-175, distance=380),
-    # Commands.ActMotorTime(speed=250, time=2400),
-    # Commands.ActMotorTime(speed=-200, time=2400),
-    # Commands.GyroDrive(speed=-175, distance=800, angle=20),
+    # Drive to M01
+    Commands.GyroDrive(speed=175, distance=-160),
+    # Do M01
+    Commands.ActMotorTime(motor=4, speed=-350, time=6250),
+    Commands.ActMotorTime(speed=350, time=2500),
+    # Wall square
+    Commands.DriveMM(speed=175, distance=200),
+    Commands.DriveMM(speed=100, distance=50),
+    # Align with M02
+    Commands.GyroDriveREG(speed=125, distance=25),
+    # Do M02
+    Commands.ActMotorTime(speed=-350, time=5555),
+    Commands.ActMotorTime(speed=350, time=4000),
+    Commands.ActMotorTime(speed=350, time=5000, wait=False),
+    # Back really outs
+    Commands.GyroDrive(speed=-175, distance=-190, angle=-5),
+    # Pivot to align with M03
+    Commands.GyroPivot(speed=80, angle=16),
+    # Drive to M03
+    Commands.GyroDrive(speed=-120, distance=25),
+    # Do M03
+    Commands.ActMotorTime(motor=1, speed=-250, time=2400),
+    Commands.ActMotorTime(motor=1, speed=200, time=2400),
+    # Go to tacos-4-life and home to regain mental sanity (not really you cant ever get it back)
+    Commands.DriveMM(speed=-175, distance=-800),
     ]
--- M08.py
+++ M08.py
@@ -3,8 +3,8 @@ import Commands
 def m08():
   return [
     Commands.ActMotorTime(motor=1, speed=200, time=2000, wait=False),
-    Commands.GyroDrive(speed=-175, distance=46),
-    Commands.GyroPivot(angle=69),
+    Commands.GyroDrive(speed=-175, distance=40),
+    Commands.GyroPivot(angle=71),
     Commands.GyroDrive(speed=-175, distance=610),
     Commands.DriveMM(speed=100, distance=5),
     Commands.ActMotorTime(motor=1, speed=-250, time=1800),
