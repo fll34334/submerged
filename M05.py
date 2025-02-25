d93a7ad213a969e4bf0bafb03323f1604768ad86
Joshua Weathers <joshua@34334.net>
2025-02-24 19:52:51 -0600

    Make M05 better

--- M05.py
+++ M05.py
@@ -12,17 +12,11 @@ def m05():
     Commands.GyroPivot(angle=-90),
     # LineSquare
     Commands.LineSquare(approachSpeed=75, returnTime=4000),
-    # Argressivly ram 13 while banging head against wall in frustraition
-    Commands.DriveMM(speed=175, distance=125),
-    Commands.DriveMM(speed=150, distance=-185),
-    # Back up to linesqare crying 
-    Commands.LineSquare(approachSpeed=75, returnTime=4000),
-    # Turn away and drop arm
     Commands.DriveMM(speed=-100, distance=-12),
     Commands.GyroPivot(angle=-90),
     # Drive to seabed
-    Commands.GyroDrive(speed=-175, distance=430),
+    Commands.GyroDrive(speed=-175, distance=440),
     Commands.GyroPivot(angle=-66),
     # Drive to home
-    Commands.DriveMM(speed=250, distance=777, angle=1),
+    Commands.DriveMM(speed=250, distance=777)
   ]
